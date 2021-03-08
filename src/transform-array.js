fafeacd183259e791dff027880f325d0aa082eab
WFZ1 <pavelskumenko@gmail.com>
2021-03-08 22:34:20 +0200

    feat: add the solution of the task 'Transform array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = arr => {
+  if (typeof(arr) !== 'object') throw new Error('arr isn\'t array!');
+
+  const controls = {
+    discard_next: '--discard-next',
+    discard_prev: '--discard-prev',
+    double_next: '--double-next',
+    double_prev: '--double-prev'
+  }
+
+  let new_arr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case controls.discard_next:
+        i++;
+        break;
+      case controls.discard_prev:
+        if (arr[i-2] !== controls.discard_next) new_arr.pop();
+        break;
+      case controls.double_next:
+        if (i !== arr.length - 1) new_arr.push(arr[i+1]);
+        break;
+      case controls.double_prev:
+        if (i !== 0 && arr[i-2] !== controls.discard_next) new_arr.push(arr[i-1]);
+        break;
+      default: new_arr.push(arr[i]);
+    }
+  }
+
+  return new_arr;
 };
